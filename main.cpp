8bf9b8529672333b6f915b88e2f6965d7cf9a36f
IsuraManchanayake <isura.14@cse.mrt.ac.lk>
2019-09-13 17:14:10 +0800

    Try to add seperate display components

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     WorkLoad wl(100000);
     std::thread t([&wl]() { wl.do_work(); });
-    ProgressBar<size_t> pb(wl.tick, wl.work);
+    ProgressBar<size_t, 1, DisplayComponent::All> pb(wl.tick, wl.work);
     pb.init();
     t.join();
 }
--- progressbar.hpp
+++ progressbar.hpp
@@ -10,29 +10,55 @@
 
 #include "windowhistory.hpp"
 
-template<typename T>
+enum class DisplayComponent {
+    None,
+    ElapsedTime,
+    RawProgress,
+    ProgressBar,
+    Percentage,
+    EstimatedTime,
+    Speed,
+    All
+};
+
+struct PrinterBase {};
+
+template<DisplayComponent dc>
+struct Printer;
+
+template<typename T, size_t N, DisplayComponent... args>
 class ProgressBar {
 public:
     ProgressBar(const T& progress, const T& max_progress) 
-        : progress(progress), max_progress(max_progress), speed_history() {}
+        : progress(progress), max_progress(max_progress), speed_history() {
+            for(size_t i = 0; i < N; i++) {
+                pbs[i] = nullptr;
+            }
+        }
 
     void init() {
         start = std::chrono::steady_clock::now();
         history.emplace(start, progress);
         while(true) {
-            print_progressbar(current_progress());
+            print_progressbar();
             std::this_thread::sleep_for(std::chrono::milliseconds(sampling_freq_millis));
             if(progress == max_progress) { 
-                print_progressbar(current_progress());
+                print_progressbar();
                 plot_speed_history();
                 break;
             }
         }
         time_point end = std::chrono::steady_clock::now();
-        std::cout << "\nJob completed in " << duration_sec(start, end) << " s" << std::endl;
+        // std::cout << "\nJob completed in " << duration_sec(start, end) << " s" << std::endl;
+    }
+    
+    ProgressBar& addComponent(const DisplayComponent& displayComponent) {
+        displayComponents.push_back(displayComponent);
+        return *this;
     }
 
 private:
+    template<DisplayComponent dc> friend struct Printer;
     typedef std::chrono::time_point<std::chrono::steady_clock> time_point;
     /* configurations */
     const size_t bar_width = 50;
@@ -51,6 +77,10 @@ private:
     std::array<double, speed_history_size> speed_history;
     double current_speed;
     time_point start;
+    time_point current;
+    std::vector<DisplayComponent> displayComponents;
+    static constexpr const DisplayComponent dcs[N] = {args...};
+    static constexpr PrinterBase* pbs[N] = {};
     /* end members */
     
     double current_progress() const {
@@ -89,25 +119,30 @@ private:
         return out;
     }
 
-    void print_progressbar(double fraction) {
+    void print_progressbar() {
         std::ostringstream ss;
         ss << std::fixed << std::setprecision(2);
-        time_point current = std::chrono::steady_clock::now();
+        current = std::chrono::steady_clock::now();
         history.emplace(current, progress);
         current_speed = calculate_speed();
         speed_history[map_progress(speed_history_size)] = current_speed;
-        double current_time = duration_sec(start, current);
-        double estimated_remaining_time = (max_progress - progress) / current_speed;
-        double current_progress_percentage = (progress * 100.0) / max_progress;
-        ss << "[Elapsed: " << fmt_time(current_time) << "] ";
-        ss << "|";
-        for(size_t i = 0; i < bar_width; i++) {
-            ss << ((i <= fraction * bar_width) ? fillchar : emptychar);
+        for(const auto& displayComponent : dcs) {
+            Printer<displayComponent>::print(ss, this);
         }
-        ss << "| ";
-        ss << current_progress_percentage << " %";
-        ss << " [Est.Remaining: " << fmt_time(estimated_remaining_time) << "]";
-        ss << " [Speed: " << current_speed << " Tick/s]";
+
+        // double current_time = duration_sec(start, current);
+        // double estimated_remaining_time = (max_progress - progress) / current_speed;
+        // double current_progress_percentage = (progress * 100.0) / max_progress;
+        // ss << "[Elapsed: " << fmt_time(current_time) << "] ";
+        // ss << "[Progress: " << progress << '/' << max_progress << " Ticks] ";
+        // ss << "|";
+        // for(size_t i = 0; i < bar_width; i++) {
+        //     ss << ((i <= (progress * bar_width) / max_progress) ? fillchar : emptychar);
+        // }
+        // ss << "| ";
+        // ss << current_progress_percentage << " %";
+        // ss << " [Est.Remaining: " << fmt_time(estimated_remaining_time) << "]";
+        // ss << " [Speed: " << current_speed << " Tick/s]";
         std::string output = ss.str();
         struct winsize size;
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
@@ -116,12 +151,43 @@ private:
         // for(size_t i = 0; i < output_lines - 1; i++) {
         //     std::cout << "\033[F";
         // }
-        std::cout << '\r' << output << std::flush;
+        std::cout << "\33[2K\r" << output << std::flush;
+    }
+
+    void print_elapsedtime(std::ostringstream& ss) const {
+        double current_time = duration_sec(start, current);
+        ss << "[Elapsed: " << fmt_time(current_time) << "]";
+    }
+
+    void print_rawprogress(std::ostringstream& ss) const {
+        ss << "[Progress: " << progress << '/' << max_progress << " Ticks]";
+    }
+
+    void print_progressbar(std::ostringstream& ss) const {
+        ss << "|";
+        for(size_t i = 0; i < bar_width; i++) {
+            ss << ((i <= (progress * bar_width) / max_progress) ? fillchar : emptychar);
+        }
+        ss << "|";
+    }
+
+    void print_percentage(std::ostringstream& ss) const {
+        double current_progress_percentage = (progress * 100.0) / max_progress;
+        ss << current_progress_percentage << " %";
+    }
+
+    void print_estimatedtime(std::ostringstream& ss) const {
+        double estimated_remaining_time = (max_progress - progress) / current_speed;
+        ss << " [Est.Remaining: " << fmt_time(estimated_remaining_time) << "]";
+    }
+
+    void print_speed(std::ostringstream& ss) const {
+        ss << " [Speed: " << current_speed << " Tick/s]";
     }
 
     void plot_speed_history() const {
         std::array<std::array<std::string, speed_history_size>, speed_history_plot_height> plot;
-        for(auto& s: plot) for(auto& c: s) c = ".";
+        for(auto& s: plot) for(auto& c: s) c = "·";
         double max_speed = 0;
         for(const auto& speed: speed_history) { 
             max_speed = (max_speed < speed && speed != std::numeric_limits<double>::infinity()) ? speed : max_speed;
@@ -143,10 +209,28 @@ private:
         for(size_t i = 0; i < speed_history_size; i++) {
             std::cout << "───";
         }
-        // std::cout << "\n  ";
-        // for(size_t i = 0; i <= speed_history_size; i++) {
-        //     std::cout << ((i * 100) / speed_history_size) << " ";
-        // }
-        // std::cout << std::flush;
+        std::cout << "\n";
+        for(size_t i = 1; i <= speed_history_size; i++) {
+            std::cout << std::setw(3) << ((i * 100) / speed_history_size) << std::flush;
+        }
+        std::cout << std::endl;
+    }
+};
+
+template<>
+struct Printer<DisplayComponent::ElapsedTime> : PrinterBase {
+    template<typename T, size_t N, DisplayComponent... args>
+    static void print(std::ostringstream& ss, const ProgressBar<T, N, args...>* pb) {
+        double current_time = duration_sec(pb->start, pb->current);
+        ss << "[Elapsed: " << ProgressBar<T, N, args...>::fmt_time(current_time) << "]";
+    }
+};
+
+template<>
+struct Printer<DisplayComponent::All> : PrinterBase {
+    template<typename T, size_t N, DisplayComponent... args>
+    static void print(std::ostringstream& ss, const ProgressBar<T, N, args...>* pb) {
+        Printer<DisplayComponent::ElapsedTime>::print(ss, pb);
+        Printer<DisplayComponent::ElapsedTime>::print(ss, pb);
     }
 };
